7cee397ee7a6ccf9c6904e1976d72b27d4683a92
leosaline <leosaline@gmail.com>
2018-03-06 22:12:22 -0300

    initial commit

---
+++ src/main/java/com/wroclaw/saline/SbConceptApplication.java
@@ -0,0 +1,12 @@
+package com.wroclaw.saline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbConceptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbConceptApplication.class, args);
+	}
+}
